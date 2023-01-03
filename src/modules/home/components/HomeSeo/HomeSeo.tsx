aec1f1057bd38d1886f20b9b77825ef24911f5de
lbavendanos <lbavendanos@gmail.com>
2023-01-03 17:31:35 -0500

    Add twitter metas

--- src/modules/home/components/HomeSeo/HomeSeo.tsx
+++ src/modules/home/components/HomeSeo/HomeSeo.tsx
@@ -6,6 +6,7 @@ export default function HomeSeo() {
   const appName = config('app.name')
   const title = appName
   const description = 'Software Developer'
+  const image = ''
 
   const metas: SeoMetas = [
     { property: 'og:title', content: title },
@@ -13,7 +14,12 @@ export default function HomeSeo() {
     { property: 'og:type', content: 'website' },
     { property: 'og:site_name', content: appName },
     { property: 'og:url', content: url() },
-    { property: 'og:image', content: '' },
+    { property: 'og:image', content: image },
+    { name: 'twitter:card', content: 'summary_large_image' },
+    { name: 'twitter:site', content: '@lbavendanos' },
+    { name: 'twitter:title', content: title },
+    { name: 'twitter:description', content: description },
+    { name: 'twitter:image', content: image },
   ]
 
   return <Seo title={title} description={description} metas={metas} />
