54dcf1bb3a108fe901f016f2d5ff2d6362264e3e
lbavendanos <lbavendanos@gmail.com>
2023-01-03 13:46:35 -0500

    Add config properties

---
+++ src/config/app.ts
@@ -0,0 +1,6 @@
+const app = {
+  name: process.env.NEXT_PUBLIC_APP_NAME || 'App name',
+  url: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost',
+}
+
+export default app
--- src/lib/utils/helpers.ts
+++ src/lib/utils/helpers.ts
@@ -1,1 +1,15 @@
+import { objGet } from './object'
+import app from '@/config/app'
+
+/**
+ * Gets the specified configuration value.
+ *
+ * @param {string} path - The path of the configuration to get.
+ * @param {any} [defaultValue] - The value returned if the resolved value is undefined.
+ * @returns {any} Returns the resolved value.
+ */
+export function config(path: string, defaultValue?: any): any {
+  return objGet({ app }, path, defaultValue)
+}
+
 export { default as cn } from 'clsx'
