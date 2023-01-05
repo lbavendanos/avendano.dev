e5c239f518cf6851984a8b1b94fc1ae26e6567ed
lbavendanos <lbavendanos@gmail.com>
2023-01-05 16:42:34 -0500

    Add blog module

--- src/modules/blog/BlogModule.tsx
+++ src/modules/blog/BlogModule.tsx
@@ -1,10 +1,15 @@
+import BlogSeo from './components/BlogSeo'
 import Container from '@/lib/components/Container'
 import BaseLayout from '@/layouts/base'
+import BlogOverview from './components/BlogOverview'
 
 export default function BlogModule() {
   return (
     <BaseLayout>
-      <Container>Blog</Container>
+      <BlogSeo />
+      <Container className="my-10">
+        <BlogOverview />
+      </Container>
     </BaseLayout>
   )
 }
---
+++ src/modules/blog/components/BlogOverview/BlogOverview.tsx
@@ -0,0 +1,12 @@
+export default function BlogOverview() {
+  return (
+    <div className="flex flex-col">
+      <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
+        Blog
+      </h1>
+      <p className="text-gray-600 dark:text-gray-400">
+        Thoughts on the software industry
+      </p>
+    </div>
+  )
+}
