5be917113ddb16d2f9b60bc50e70d501bfbebd66
lbavendanos <lbavendanos@gmail.com>
2023-01-08 22:12:38 -0500

    Add blank and filled helpers

--- src/lib/utils/helpers.ts
+++ src/lib/utils/helpers.ts
@@ -14,4 +14,39 @@ export function config<T = any>(path: string, defaultValue?: T): T {
   return objGet({ app, spotify }, path, defaultValue)
 }
 
+/**
+ * Determine if the given value is "blank".
+ *
+ * @template T - The type of value
+ * @param {T} value - The value has checked
+ * @returns {boolean} Return the value status
+ */
+export function blank<T>(value: T): boolean {
+  const type = Object.prototype.toString.call(value)
+
+  if (type === '[object Undefined]' || type === '[object Null]') return true
+
+  if (type === '[object Number]' || type === '[object Boolean]') return false
+
+  if (type === '[object String]') return (value as string).trim() === ''
+
+  if (type === '[object Array]' || type === '[object Object]')
+    return Array.isArray(value)
+      ? value.length === 0
+      : Object.keys(value as object).length === 0
+
+  return !value
+}
+
+/**
+ * Determine if a value is "filled".
+ *
+ * @template T - The type of value
+ * @param {T} value - The value has checked
+ * @returns {boolean} Return the value status
+ */
+export function filled<T>(value: T): boolean {
+  return !blank(value)
+}
+
 export { default as cn } from 'clsx'
